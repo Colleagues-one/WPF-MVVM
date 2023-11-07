f472ee5198ab25ccabe42ff5f591570ad75c5e88
Kirill Kravchenko <kirillkravchenko89@gmail.com>
2023-11-07 13:17:56 +0700

    получение данных по датам

--- src/WPF-MVVM/WPF-MVVM-Console/Program.cs
+++ src/WPF-MVVM/WPF-MVVM-Console/Program.cs
@@ -1,20 +1,52 @@
-﻿using System.Net;
+﻿using System.Globalization;
+using System.Net;
+using System.Runtime.InteropServices.JavaScript;
 using static System.Net.WebRequestMethods;
 
 namespace WPF_MVVM_Console
 {
     internal class Program
     {
-        private const string dataURL = @"https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+        private const string dataURL = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
         static void Main(string[] args)
-        {  
-            HttpClient client = new HttpClient();
-            var response = client.GetAsync(dataURL).Result;
-            var csv_str = response.Content.ReadAsStringAsync().Result;
-
+        {
+            
+            /*foreach (var data_line in GetDataLines())
+            {
+                Console.WriteLine(data_line);
+            }*/
+            var dates = GetDates();
+            Console.WriteLine(string.Join("\r\n", dates));
             
             Console.ReadKey();
         }
+
+
+        private static async Task<Stream> GetDataStream()
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(dataURL, HttpCompletionOption.ResponseHeadersRead);
+            return await response.Content.ReadAsStreamAsync();
+        }
+
+        private static IEnumerable<string> GetDataLines()
+        {
+            using var data_stream = GetDataStream().Result;
+            using var data_reader = new StreamReader(data_stream);
+            while (!data_reader.EndOfStream)
+            {
+                var line = data_reader.ReadLine();
+                if(string.IsNullOrWhiteSpace(line)) continue;
+                yield return line;
+            }
+        }
+
+        private static DateTime[] GetDates() => GetDataLines()
+            .First()
+            .Split(',')
+            .Skip(4)
+            .Select(s=>DateTime.Parse(s,CultureInfo.InvariantCulture))
+            .ToArray();
     }
 }
