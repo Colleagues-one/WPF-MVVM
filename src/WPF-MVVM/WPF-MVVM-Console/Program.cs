a401eeda11580652d8f481c67d6088a4e26c4c0d
Kirill Kravchenko <kirillkravchenko89@gmail.com>
2023-11-07 11:38:40 +0700

    запрос к удаленному серверу в тесковом консольном приложении

--- src/WPF-MVVM/WPF-MVVM-Console/Program.cs
+++ src/WPF-MVVM/WPF-MVVM-Console/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace WPF_MVVM_Console
+﻿using System.Net;
+using static System.Net.WebRequestMethods;
+
+namespace WPF_MVVM_Console
 {
     internal class Program
     {
+        private const string dataURL = @"https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {  
+            HttpClient client = new HttpClient();
+            var response = client.GetAsync(dataURL).Result;
+            var csv_str = response.Content.ReadAsStringAsync().Result;
+
+            
+            Console.ReadKey();
         }
     }
 }
