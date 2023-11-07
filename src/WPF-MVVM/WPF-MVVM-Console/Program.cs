dbb90988531444eaac1fb3d373ad2fe6492ba36c
Kirill Kravchenko <kirillkravchenko89@gmail.com>
2023-11-07 15:00:49 +0700

    тестое отображение данных

--- src/WPF-MVVM/WPF-MVVM-Console/Program.cs
+++ src/WPF-MVVM/WPF-MVVM-Console/Program.cs
@@ -7,18 +7,21 @@ namespace WPF_MVVM_Console
 {
     internal class Program
     {
-        private const string dataURL = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+        private const string dataURL =
+            @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
         static void Main(string[] args)
         {
-            
+
             /*foreach (var data_line in GetDataLines())
             {
                 Console.WriteLine(data_line);
             }*/
-            var dates = GetDates();
-            Console.WriteLine(string.Join("\r\n", dates));
-            
+           /* var dates = GetDates();
+            Console.WriteLine(string.Join("\r\n", dates));*/
+
+           var russia = GetData().First(v => v.Country.Equals("Russia", StringComparison.OrdinalIgnoreCase));
+           Console.WriteLine(string.Join("\r\n", GetDates().Zip(russia.Counts,(date,count) => $"{date:dd:MM} - {count}")));
             Console.ReadKey();
         }
 
@@ -37,8 +40,8 @@ namespace WPF_MVVM_Console
             while (!data_reader.EndOfStream)
             {
                 var line = data_reader.ReadLine();
-                if(string.IsNullOrWhiteSpace(line)) continue;
-                yield return line;
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                yield return line.Replace("Korea,", "Korea-");
             }
         }
 
@@ -46,7 +49,25 @@ namespace WPF_MVVM_Console
             .First()
             .Split(',')
             .Skip(4)
-            .Select(s=>DateTime.Parse(s,CultureInfo.InvariantCulture))
+            .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
             .ToArray();
+
+        private static IEnumerable<(string Country, string Province, int[] Counts)> GetData()
+        {
+            var lines = GetDataLines()
+                .Skip(1)
+                .Select(line => line.Split(','));
+            foreach (var row in lines)
+            {
+                var province = row[0].Trim();
+                var country_name = row[1].Trim(' ', '"');
+                var counts = row.Skip(5)
+                    .Select(int.Parse).ToArray();
+                yield return(country_name, province, counts);
+            }
+        }
     }
 }
+
+
+
--- src/WPF-MVVM/WPF-MVVM/ViewModels/MainWindowViewModel.cs
+++ src/WPF-MVVM/WPF-MVVM/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,29 @@
-﻿using System.Windows;
+﻿using System.Net.NetworkInformation;
+using System.Windows;
 using System.Windows.Input;
 using WPF_MVVM.Infrastructure.Commands;
+using WPF_MVVM.Models;
 using WPF_MVVM.ViewModels.Base;
 
 namespace WPF_MVVM.ViewModels
 {
     internal class MainWindowViewModel : BaseViewModel
     {
+
+        #region TestDataPoints :IEnumerable<DataPoint> - Тестовый
+        /// <summary>
+        /// Тестовый набор дданных визуализации данных
+        /// </summary>
+        private IEnumerable<DatePoint> _testDataPoints;
+        /// <summary>
+        /// Тестовый набор дданных визуализации данных
+        /// </summary>
+        public IEnumerable<DatePoint> TestDatePoints
+        {
+            get => _testDataPoints;
+            set => Set(ref _testDataPoints, value);
+        } 
+        #endregion
 
         #region Title Заголовок окна
         private string _title = "MainWindow";
@@ -56,6 +73,15 @@ namespace WPF_MVVM.ViewModels
                 new RelayCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
 
             #endregion
+            var data_Points = new List<DatePoint>((int)(360/0.1));
+            for (var x = 0d; x <= 360; x += 0.1)
+            {
+                const double to_rad = Math.PI / 180;
+                var y = Math.Sin(x * to_rad);
+                data_Points.Add((new DatePoint{XValue = x, YValue = y}));
+            }
+
+            TestDatePoints = data_Points;
         }
     }
 }
