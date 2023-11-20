ed9c9537e1d16e643b5a80ff521262b60fc1c82d
Kirill Kravchenko <kirillkravchenko89@gmail.com>
2023-11-20 12:15:21 +0700

    фильтрация плохо реализованная прямо в View

--- src/WPF-MVVM/WPF-MVVM/Views/Windows/MainWindow.xaml.cs
+++ src/WPF-MVVM/WPF-MVVM/Views/Windows/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPF_MVVM.Models.Decanat;
 
 namespace WPF_MVVM
 {
@@ -19,6 +21,28 @@ namespace WPF_MVVM
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GroupsCollection_OnFilter(object sender, FilterEventArgs e)
+        {
+            if (!(e.Item is Models.Decanat.Group group)) return;
+            if (group.Name is null) return;
+
+            var filter = GroupNameFilterText.Text;
+
+            if(filter.Length == 0) return;
+
+            if(group.Name.Contains(filter, StringComparison.OrdinalIgnoreCase)) return;
+            if(group.Description != null && group.Description.Contains(filter, StringComparison.OrdinalIgnoreCase)) return;
+
+            e.Accepted = false;
+        }
+
+        private void OnGroupsFilterTextChanged(object sender, TextChangedEventArgs e)
+        {   
+            var textbox = (TextBox)sender;
+            var collection = (CollectionViewSource)textbox.FindResource("GroupsCollection");
+            collection.View.Refresh();
         }
     }
 }
