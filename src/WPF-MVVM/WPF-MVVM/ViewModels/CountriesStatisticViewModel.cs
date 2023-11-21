3c1633e8795947dce7015dc744d30b97e5ffbfd4
Kirill Kravchenko <kirillkravchenko89@gmail.com>
2023-11-22 00:06:53 +0700

    базовая логика ViewModel Для статистических данных

--- src/WPF-MVVM/WPF-MVVM/ViewModels/CountriesStatisticViewModel.cs
+++ src/WPF-MVVM/WPF-MVVM/ViewModels/CountriesStatisticViewModel.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using WPF_MVVM.Infrastructure.Commands;
+using WPF_MVVM.Models;
 using WPF_MVVM.Services;
 using WPF_MVVM.ViewModels.Base;
 
@@ -14,11 +17,54 @@ namespace WPF_MVVM.ViewModels
 
         private DataService _dataService;
 
+
+
+        #region Countries : IEnumerable<CountryInfo> - Статистика по странам
+
+        /// <summary>
+        /// field Статистика по странам
+        /// </summary>
+        private IEnumerable<CountryInfo> _Countries;
+
+        /// <summary>
+        ///  attribute Статистика по странам
+        /// </summary>
+        public IEnumerable<CountryInfo> Countries
+        {
+            get => _Countries;
+            private set => Set(ref _Countries, value);
+        }
+
+        #endregion
+
+
+
+        #region ICommand RefreshData (object) - Команда обновления данных
+
+        /// <summary>
+        /// Команда обновления данных
+        /// </summary>
+        public ICommand RefreshDataCommand { get; }
+
+        private bool CanRefreshDataCommandExecute(object parameter) => true;
+
+        private void OnRefreshDataCommandExecuted(object parameter)
+        {
+            Countries = _dataService.GetData();
+        }
+
+        #endregion
+
+
         public CountriesStatisticViewModel(MainWindowViewModel MainModel)
         {
+
+
             MainWindowViewModel = MainModel;
 
             _dataService = new DataService();
+
+            RefreshDataCommand = new RelayCommand(OnRefreshDataCommandExecuted, CanRefreshDataCommandExecute);
         }
 
     }
