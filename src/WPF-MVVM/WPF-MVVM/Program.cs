77271c1970732b8b06d4923041cbe1c6f5bfc207
Kirill Kravchenko <kirillkravchenko89@gmail.com>
2024-01-23 14:25:59 +0700

    Создание собственной точки входа Program.Main()

---
+++ src/WPF-MVVM/WPF-MVVM/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_MVVM
+{
+    public static class Program 
+    {
+        [System.STAThreadAttribute()]
+        [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
+        public static void Main()
+        {
+            WPF_MVVM.App app = new WPF_MVVM.App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
