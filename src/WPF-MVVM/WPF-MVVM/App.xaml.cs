d3d3466c54f5a514d0067a197ec6940099fa96f9
Kirill Kravchenko <kirillkravchenko89@gmail.com>
2024-01-23 14:35:25 +0700

    Инфраструктура для создания хоста приложения

--- src/WPF-MVVM/WPF-MVVM/App.xaml.cs
+++ src/WPF-MVVM/WPF-MVVM/App.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using WPF_MVVM.Services;
+using WPF_MVVM.ViewModels;
 
 namespace WPF_MVVM
 {
@@ -20,8 +23,15 @@ namespace WPF_MVVM
             /*var service_test = new DataService();
             var countries = service_test.GetData().ToArray();*/
         }
+
+
+
+
+        public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddSingleton<DataService>();
+            services.AddSingleton<CountriesStatisticViewModel>();
+
+        }
     }
-
- 
-
 }
--- src/WPF-MVVM/WPF-MVVM/Program.cs
+++ src/WPF-MVVM/WPF-MVVM/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace WPF_MVVM
 {
@@ -17,6 +20,23 @@ namespace WPF_MVVM
             WPF_MVVM.App app = new WPF_MVVM.App();
             app.InitializeComponent();
             app.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var host_builder = Host.CreateDefaultBuilder(args);
+
+            host_builder.UseContentRoot(Environment.CurrentDirectory);
+            host_builder.ConfigureAppConfiguration((host, cfg) =>
+            {
+                cfg.SetBasePath(Environment.CurrentDirectory);
+                cfg.AddJsonFile("appsettings.json", optional: true, reloadOnChange:true);
+            });
+
+            host_builder.ConfigureServices(App.ConfigureServices);
+
+
+            return host_builder;
         }
     }
 }
