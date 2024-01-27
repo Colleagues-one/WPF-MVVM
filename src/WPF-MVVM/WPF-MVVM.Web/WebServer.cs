b422dc815a6a1bbe3efe00eaf115225ffdb0e255
Kirill Kravchenko <kirillkravchenko89@gmail.com>
2024-01-27 23:34:18 +0700

    базовая реализация webServer

--- src/WPF-MVVM/WPF-MVVM.Web/WebServer.cs
+++ src/WPF-MVVM/WPF-MVVM.Web/WebServer.cs
@@ -6,12 +6,14 @@ namespace WPF_MVVM.Web
 {
     public class WebServer
     {
+        private event EventHandler<RequestReceiverEventArgs> RequestReceiver; 
         private HttpListener? _httpListener;
         private readonly int _port;
         private bool _enabled;
         private readonly object _syncRoot = new object();
 
         public int Port => _port;
+
         public bool Enabled
         {
             get => _enabled;
@@ -38,23 +40,44 @@ namespace WPF_MVVM.Web
                 _httpListener.Prefixes.Add($"http://+:{_port}");
                 _enabled = true;
             }
-            Listen();
+            //ListenAsync();
         }
 
         public void Stop()
         {
-            if(! _enabled) return;
+            if (!_enabled) return;
             lock (_syncRoot)
             {
-                if( _enabled) return;
+                if (_enabled) return;
                 _httpListener = null;
                 _enabled = false;
             }
         }
 
-        private void Listen()
+        private async void ListenAsync()
         {
+            if(_httpListener == null) return;
+            var listener = _httpListener;
+            listener.Start();
+          
+            while (_enabled)
+            {
+                var receivedContext = await listener.GetContextAsync().ConfigureAwait(false);
+                ProcessRequest(receivedContext);
+            }
 
+            listener.Stop();
         }
+
+        private void ProcessRequest(HttpListenerContext context)
+        {
+            RequestReceiver?.Invoke(this, new RequestReceiverEventArgs(context));
+        }
+    }
+    public class RequestReceiverEventArgs : EventArgs
+    {
+        public HttpListenerContext Context { get; }
+
+        public RequestReceiverEventArgs(HttpListenerContext context) => Context = context;
     }
 }
