c9904ee0e725e8f9ad2bab6630b69457f3af3d73
Kirill Kravchenko <kirillkravchenko89@gmail.com>
2024-01-27 23:15:07 +0700

    заготовка ассинхронного сервера

--- src/WPF-MVVM/WPF-MVVM.Web/WebServer.cs
+++ src/WPF-MVVM/WPF-MVVM.Web/WebServer.cs
@@ -1,7 +1,60 @@
-﻿namespace WPF_MVVM.Web
+﻿using System.Dynamic;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WPF_MVVM.Web
 {
     public class WebServer
     {
+        private HttpListener? _httpListener;
+        private readonly int _port;
+        private bool _enabled;
+        private readonly object _syncRoot = new object();
 
+        public int Port => _port;
+        public bool Enabled
+        {
+            get => _enabled;
+            set
+            {
+                if (value)
+                    Start();
+                else Stop();
+            }
+        }
+
+
+        public WebServer(int port) => _port = port;
+
+
+        public void Start()
+        {
+            if (_enabled) return;
+            lock (_syncRoot)
+            {
+                if (_enabled) return;
+                _httpListener = new HttpListener();
+                _httpListener.Prefixes.Add($"http://*:{_port}");
+                _httpListener.Prefixes.Add($"http://+:{_port}");
+                _enabled = true;
+            }
+            Listen();
+        }
+
+        public void Stop()
+        {
+            if(! _enabled) return;
+            lock (_syncRoot)
+            {
+                if( _enabled) return;
+                _httpListener = null;
+                _enabled = false;
+            }
+        }
+
+        private void Listen()
+        {
+
+        }
     }
 }
