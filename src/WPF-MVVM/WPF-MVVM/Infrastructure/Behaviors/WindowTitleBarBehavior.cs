523b746ad4de9c934bcef36928f4588c36541533
Kirill Kravchenko <kirillkravchenko89@gmail.com>
2024-01-29 23:38:28 +0700

    добабвление расширения для поиска элемента среди родительских для визуальных и логических

---
+++ src/WPF-MVVM/WPF-MVVM/Infrastructure/Behaviors/WindowTitleBarBehavior.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+
+namespace WPF_MVVM.Infrastructure.Behaviors
+{
+    internal class WindowTitleBarBehavior : Behavior<UIElement>
+    {
+        private Window _window;
+
+
+        protected override void OnAttached()
+        {
+            _window = AssociatedObject as Window ?? AssociatedObject.Find;
+        }
+
+        protected override void OnDetaching()
+        {
+
+        }
+    }
+    
+}
--- src/WPF-MVVM/WPF-MVVM/Infrastructure/Extensions/DependencyObjectExtensions.cs
+++ src/WPF-MVVM/WPF-MVVM/Infrastructure/Extensions/DependencyObjectExtensions.cs
@@ -18,6 +18,7 @@ namespace System.Windows
                 obj = parent;
             } while (true);
         }
+
         public static DependencyObject FindLogicalRoot(this DependencyObject obj)
         {
             do
@@ -26,6 +27,28 @@ namespace System.Windows
                 if (parent is null) return obj;
                 obj = parent;
             } while (true);
+        }
+
+        public static DependencyObject FindVisualParent<T>(this DependencyObject obj) where T : DependencyObject
+        {
+            if(obj is null) return null;
+            var target = obj;
+            do
+            {
+                target = VisualTreeHelper.GetParent(target);
+            } while (target != null && target is not T);
+            return target as T;
+        }
+
+        public static DependencyObject FindLogicalParent<T>(this DependencyObject obj) where T : DependencyObject
+        {
+            if (obj is null) return null;
+            var target = obj;
+            do
+            {
+                target = LogicalTreeHelper.GetParent(target);
+            } while (target != null && target is not T);
+            return target as T;
         }
     }
 }
