65f9a960efb9da502d8e49faaa59d7f5b17f892c
Kirill Kravchenko <kirillkravchenko89@gmail.com>
2024-01-29 22:44:21 +0700

    методы расшитения для dependencyObject, позволяющие найти корень в дереве, создание behaivors в основно приложении

---
+++ src/WPF-MVVM/WPF-MVVM/Infrastructure/Behaviors/WindowStateChange.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Microsoft.Xaml.Behaviors;
+
+namespace WPF_MVVM.Infrastructure.Behaviors
+{
+    internal class WindowStateChange : Behavior<Button> 
+    {
+
+    }
+}
