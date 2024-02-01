bf0d39aaf79782b74db5582a103eb0a0d91edba7
Kirill Kravchenko <kirillkravchenko89@gmail.com>
2024-02-01 17:27:00 +0700

    Комманда открытия окна управления студентами

---
+++ src/WPF-MVVM/WPF-MVVM/Infrastructure/Commands/ManageStudentsCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WPF_MVVM.Infrastructure.Commands.Base;
+using WPF_MVVM.Views;
+
+namespace WPF_MVVM.Infrastructure.Commands
+{
+    internal class ManageStudentsCommand : Command
+    {
+        private StudentsManagementWindow _window;
+
+        public override bool CanExecute(object parameter) => _window == null;
+
+        public override void Execute(object parameter)
+        {
+            var window = new StudentsManagementWindow { Owner = Application.Current.MainWindow };
+            _window = window;
+            window.Closed += OnWindowClosed;
+            window.ShowDialog();
+
+        }
+
+        private void OnWindowClosed(object sender, EventArgs e)
+        {
+            ((Window)sender).Closed -= OnWindowClosed;
+            _window = null;
+        }
+    }
+}
